093b84112e19a2ad54ca3282726147bc7198b8f2
Ryan Leung <rleungx@gmail.com>
2025-01-21 11:35:17 +0800

    *: check file header (#9015)
    ref tikv/pd#4322
    
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- client/resource_group/controller/limiter.go
+++ client/resource_group/controller/limiter.go
@@ -1,7 +1,3 @@
-// Copyright 2015 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 // Copyright 2023 TiKV Project Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package controller
 
--- pkg/window/counter_test.go
+++ pkg/window/counter_test.go
@@ -1,6 +1,3 @@
-// The MIT License (MIT)
-// Copyright (c) 2022 go-kratos Project Authors.
-//
 // Copyright 2023 TiKV Project Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// The MIT License (MIT)
+// Copyright (c) 2022 go-kratos Project Authors.
 
 package window
 
